2c3c81e4fef2892dd8ac2608207cb7483b476a1d
Prem Kumar <112409302+prem-kumar-sharma@users.noreply.github.com>
2024-08-22 00:00:03 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import '../App.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar-links">
-        <li><Link to="/goals" className="navbar-btn">Set Goals</Link></li>  {/* Moved to first place */}
-        <li><Link to="/" className="navbar-btn">Home</Link></li>
-        <li><Link to="/search" className="navbar-btn">Search Food</Link></li>
-        <li><Link to="/log" className="navbar-btn">Food Log</Link></li>
-      </ul>
+      <div className="navbar-container">
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
+          <li><Link to="/goals" className="navbar-btn" onClick={toggleMenu}>Set Goals</Link></li>
+          <li><Link to="/" className="navbar-btn" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/search" className="navbar-btn" onClick={toggleMenu}>Search Food</Link></li>
+          <li><Link to="/log" className="navbar-btn" onClick={toggleMenu}>Food Log</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 };
